30981896ff9e29861d70306ab1e0398241c069e1
Kehinde Ohiosumuan <ohiosumuankehinde@gmail.com>
2021-05-10 20:53:39 +0100

    new commits

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g)g)g$-+9$ro!kwcr@p9h@d+$nljtysf38tm=emb2hr@g+=mnq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dreambloggerz.herokuapp.com/', 'http://127.0.0.1:8000/']
 
 
 # Application definition
